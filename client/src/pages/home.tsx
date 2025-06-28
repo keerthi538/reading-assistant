44021e5a6df6d5513020663ad256fb5f72b7b000
keerthi538 <keerthikv.dev@gmail.com>
2025-06-28 18:47:27 +0530

    feat: add pdf display code  - User can now upload pdf files to the application and view the file on    screen  - Text content can be selected and accessed from code(need this in the    future)  - Kind of a stable UI now, hope for best:)
    relates to #1

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,13 +1,20 @@
-import Navbar from '@/components/navbar';
-import PDFViewer from '@/components/pdf-viewer';
-import ChatInterface from '@/components/chat-interface';
+import Navbar from "@/components/navbar";
+import PDFViewer from "@/components/pdf-viewer";
+import ChatInterface from "@/components/chat-interface";
+import { useState } from "react";
 
 export default function Home() {
+  const [selectedText, setSelectedText] = useState("");
+
+  const handleTextSelection = (text: string) => {
+    setSelectedText(text);
+  };
+
   return (
     <div className="h-screen overflow-hidden bg-slate-50">
       <Navbar />
       <div className="flex h-[calc(100vh-73px)]">
-        <PDFViewer />
+        <PDFViewer handleTextSelection={handleTextSelection} />
         <ChatInterface />
       </div>
     </div>
